c1ee11d877fdd40638efaa5ad138c5a8c2bdf787
ESEJuchanKim <44192669+ESEJuchanKim@users.noreply.github.com>
2018-11-29 01:34:04 +0900

    Update server.cpp

--- server_practice/server.cpp
+++ server_practice/server.cpp
@@ -1,74 +1,117 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "time.h"
-#include "sys/types.h"
-#include "sys/socket.h"
-#include "netinet/in.h"
-//소켓 프로그래밍에 사용될 헤더파일 선언
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <srpa/inet.h>
+
+#include <sys/socket.h>
+
  
-#define BUF_LEN 128
-//메시지 송수신에 사용될 버퍼 크기를 선언
+
+void error_handling(char *message);
+
  
+
 int main(int argc, char *argv[])
+
 {
-    char buffer[BUF_LEN];
-    struct sockaddr_in server_addr, client_addr;
-    char temp[20];
-    int server_fd, client_fd;
-    //server_fd, client_fd : 각 소켓 번호
-    int len, msg_size;
+
+        int serv_sock;
+
+        int clnt_sock;
+
  
-    if(argc != 2)
-    {
-        printf("usage : %s [port]\n", argv[0]);
-        exit(0);
-    }
+
+        struct sockaddr_in serv_addr;
+
+        struct sockaddr_in clnt_addr;
+
+        socklen_t clnt_addr_size;
+
  
-    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {// 소켓 생성
-        printf("Server : Can't open stream socket\n");
-        exit(0);
-    }
-    memset(&server_addr, 0x00, sizeof(server_addr));
-    //server_Addr 을 NULL로 초기화
+
+        char message[] = "Hello World!";
+
  
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(atoi(argv[1]));
-    //server_addr 셋팅
- 
-    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <0)
-    {//bind() 호출
-        printf("Server : Can't bind local address.\n");
-        exit(0);
-    }
- 
-    if(listen(server_fd, 5) < 0)
-    {//소켓을 수동 대기모드로 설정
-        printf("Server : Can't listening connect.\n");
-        exit(0);
-    }
- 
-    memset(buffer, 0x00, sizeof(buffer));
-    printf("Server : wating connection request.\n");
-    len = sizeof(client_addr);
-    while(1)
-    {
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
-        if(client_fd < 0)
+
+        if (argc != 2)
+
         {
-            printf("Server: accept failed.\n");
-            exit(0);
+
+               printf("Usage:%s <port>\n", argv[0]);
+
+               exit(1);
+
         }
-        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
-        printf("Server : %s client connected.\n", temp);
-    
-        msg_size = read(client_fd, buffer, 1024);
-        write(client_fd, buffer, msg_size);
-        close(client_fd);
-        printf("Server : %s client closed.\n", temp);
-    }
-    close(server_fd);
-    return 0;
+
+ 
+
+        serv_sock = socket(PF_INET, SOCK_STREAM, 0); //socket 함수호출을 통해서 소켓을 생성하고 있다
+
+        if (serv_sock == -1)
+
+               error_handling("socket() error");
+
+ 
+
+        memset(&serv_addr, 0, sizeof(serv_addr));
+
+        serv_addr.sin_family = AF_INET;
+
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        serv_addr.sin_port = htons(atoi(argv[1]));
+
+ 
+
+        if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) //bind 함수호출을 통해서 IP주소와 PORT번호를 할당하고 있다
+
+               error_handling("bind() error");
+
+ 
+
+        if (listen(serv_sock, 5) == -1) //listen 함수를 호출하고 있다. 이로써 소켓은 연결요청을 받아들일 수 있는 상태가 된다
+
+               error_handling("listen() error");
+
+ 
+
+        clnt_addr_size = sizeof(clnt_addr);
+
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size); //연결요청의 수락을 위한 accept 함수를 호출하고 있다
+
+        //연결요청이 없는 상태에서 이 함수가 호출되면, 연결요청이 있을 때까지 함수는 반환하지 않는다
+
+        if (clnt_sock == -1)
+
+               error_handling("accept() error");
+
+ 
+
+        write(clnt_sock, message, sizeof(message)); //write 함수는 데이터를 전송하는 기능의 함수인데, 이 문장이 실행됬다는 것은 연결요청이 있었다는 뜻
+
+        close(clnt_sock);
+
+        close(serv_sock);
+
+        return 0;
+
+}
+
+ 
+
+void error_handling(char *message)
+
+{
+
+        fputs(message, stderr);
+
+        fputc('\n', stderr);
+
+        exit(1);
+
 }
