bb3520117c3fd50b0779c8c9c1b90577fc4ec4ca
ESEJuchanKim <44192669+ESEJuchanKim@users.noreply.github.com>
2018-11-29 01:35:03 +0900

    Update client.cpp

--- server_practice/client.cpp
+++ server_practice/client.cpp
@@ -1,59 +1,101 @@
-/*
-daytime 클라이언트 프로그램이 실행되기 위해서는
-서버쪽에서 xinetd의 daytime이 구동하고 있어야한다.
-coder YoWu
-*/
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+#include <sys/socket.h>
+
  
-#include "stdio.h"
-#include "sys/types.h"
-#include "sys/socket.h"
-#include "netinet/in.h"
+
+void error_handling(char *message);
+
  
-#define BUF_LEN 128
- 
-void main(int argc, char *argv[])
+
+int main(int argc, char *argv[])
+
 {
-        int s, n;
-        char *haddr;
-        struct sockaddr_in server_addr;
-        //struct sockaddr_in server_addr : 서버의 소켓주소 구조체
-        char buf[BUF_LEN+1];
+
+        int sock;
+
+        struct sockaddr_in serv_addr;
+
+        char message[30];
+
+        int str_len;
+
  
-        if(argc != 2)
+
+        if (argc != 3)
+
         {
-                printf("usage : %s ip_Address\n", argv[0]);
-                exit(0);
+
+               printf("Usage: %s <IP> <port>\n", argv[0]);
+
+               exit(1);
+
         }
-        haddr = argv[1];
+
  
-        if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-        {//소켓 생성과 동시에 소켓 생성 유효검사
-                printf("can't create socket\n");
-                exit(0);
-        }
+
+        sock = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성하고 있다.
+
+        //소켓을 생성하는 순간에는 서버 소켓과 클라이언트 소켓으로 나뉘지 않는다.
+
+        //bind listen함수의 호출이 이어지면 서버 소켓, connect 함수의 호출로 이어지면 클라이언트 소켓
+
+        if (sock == -1)
+
+               error_handling("socket() error");
+
  
-        bzero((char *)&server_addr, sizeof(server_addr));
-        //서버의 소켓주소 구조체 server_addr을 NULL로 초기화
+
+        memset(&serv_addr, 0, sizeof(serv_addr));
+
+        serv_addr.sin_family = AF_INET;
+
+        serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+        serv_addr.sin_port = htons(atoi(argv[2]));
+
  
-        server_addr.sin_family = AF_INET;
-        //주소 체계를 AF_INET 로 선택
-        server_addr.sin_addr.s_addr = inet_addr(argv[1]);
-        //32비트의 IP주소로 변환
-        server_addr.sin_port = htons(13);
-        //daytime 서비스 포트 번호
+
+        if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) //connect 함수호출을 통해서 서버 프로그램에 연결 요청
+
+               error_handling("connect() error");
+
  
-        if(connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-        {//서버로 연결요청
-                printf("can't connect.\n");
-                exit(0);
-        }
+
+        str_len = read(sock, message, sizeof(message) - 1);
+
+        if (str_len == -1)
+
+               error_handling("read() error");
+
  
-        while((n = read(s, buf, BUF_LEN)) > 0)
-        {//서버가 보내오는 daytime 데이터의 수신
-                buf[n] = NULL;
-                printf("%s", buf);
-        }
+
+        printf("Message from server:%s\n", message);
+
+        close(socket);
+
+        return 0;
+
+}
+
  
-        close(s);
-        //사용이 완료된 소켓을 닫기
+
+void error_handling(char *message)
+
+{
+
+        fputs(message, stderr);
+
+        fputc('\n', stderr);
+
+        exit(1);
+
 }
