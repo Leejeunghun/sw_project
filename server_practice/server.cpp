fbe6c3bf6e526b02ba552ad268a187a9262b6e8f
ESEJuchanKim <44192669+ESEJuchanKim@users.noreply.github.com>
2018-11-29 01:20:23 +0900

    Update server.cpp

--- server_practice/server.cpp
+++ server_practice/server.cpp
@@ -1,167 +1,74 @@
-#include "stdafx.h"
-
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <winsock2.h>
-
-
-
-void ErrorHandling(char* message);
-
-
-
-struct socketStruct {
-
-   int nrData;
-
-   double data[100];
-
-};
-
-
-
-int _tmain(int argc, _TCHAR* argv[])
-
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "time.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "netinet/in.h"
+//소켓 프로그래밍에 사용될 헤더파일 선언
+ 
+#define BUF_LEN 128
+//메시지 송수신에 사용될 버퍼 크기를 선언
+ 
+int main(int argc, char *argv[])
 {
-
-	int portNum = 1234;
-
-	// 보낼 데이터를 초기화한다. 
-
-	socketStruct txData, TermData;
-
-	txData.nrData = 3;
-
-	for(int i = 0; i<txData.nrData; i++)
-
-	{
-
-		txData.data[i] = i*10;
-
-	}
-
-	TermData.nrData = 0;
-
-
-
-	WSADATA wsaData;
-
-	SOCKET hServSock, hClntSock;
-
-	SOCKADDR_IN servAddr, clntAddr;
-
-
-
-	int szClntAddr;
-
-	char message[]="Hello World!";
-
-	
-
-	if(WSAStartup(MAKEWORD(2, 2), &wsaData)!=0)
-
-		ErrorHandling("WSAStartup() error!");
-
-
-
-	hServSock=socket(PF_INET, SOCK_STREAM, 0);
-
-	if(hServSock==INVALID_SOCKET)
-
-		ErrorHandling("socket() error");
-
-
-
-	memset(&servAddr, 0, sizeof(servAddr));
-
-	servAddr.sin_family=AF_INET;
-
-	servAddr.sin_addr.s_addr=htonl(INADDR_ANY);
-
-	servAddr.sin_port = htons(portNum);
-
-
-
-	if(bind(hServSock, (SOCKADDR*) &servAddr, sizeof(servAddr))==SOCKET_ERROR)
-
-		ErrorHandling("bind() error");
-
-
-
-	if(listen(hServSock, 5)==SOCKET_ERROR)
-
-		ErrorHandling("listen() error");
-
-
-
-	szClntAddr=sizeof(clntAddr);
-
-	// 아래 함수에서 holding하고 있는다. 
-
-	printf("[Server] Holding at accept \n");
-
-	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
-
-	if(hClntSock==INVALID_SOCKET)
-
-		ErrorHandling("accept() error");
-
-
-
-	// 보내는 부분이다. 
-
-	int maxCnt = 5;
-
-	while(1)
-
-	{
-
-		send(hClntSock, (char*)&txData, sizeof(txData), 0);
-
-		Sleep(100);
-
-		if(maxCnt-- < 0)
-
-		{
-
-			break;
-
-		}
-
-	}
-
-	send(hClntSock, (char*)&TermData, sizeof(TermData), 0);
-
-	
-
-	// 소켓 종료 
-
-	printf("Closing Socket \n");
-
-	closesocket(hClntSock);
-
-	closesocket(hServSock);
-
-	WSACleanup();
-
-	return 0;
-
-
-
-}
-
-
-
-void ErrorHandling(char* message)
-
-{
-
-	fputs(message, stderr);
-
-	fputc('\n', stderr);
-
-	exit(1);
-
+    char buffer[BUF_LEN];
+    struct sockaddr_in server_addr, client_addr;
+    char temp[20];
+    int server_fd, client_fd;
+    //server_fd, client_fd : 각 소켓 번호
+    int len, msg_size;
+ 
+    if(argc != 2)
+    {
+        printf("usage : %s [port]\n", argv[0]);
+        exit(0);
+    }
+ 
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {// 소켓 생성
+        printf("Server : Can't open stream socket\n");
+        exit(0);
+    }
+    memset(&server_addr, 0x00, sizeof(server_addr));
+    //server_Addr 을 NULL로 초기화
+ 
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(atoi(argv[1]));
+    //server_addr 셋팅
+ 
+    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <0)
+    {//bind() 호출
+        printf("Server : Can't bind local address.\n");
+        exit(0);
+    }
+ 
+    if(listen(server_fd, 5) < 0)
+    {//소켓을 수동 대기모드로 설정
+        printf("Server : Can't listening connect.\n");
+        exit(0);
+    }
+ 
+    memset(buffer, 0x00, sizeof(buffer));
+    printf("Server : wating connection request.\n");
+    len = sizeof(client_addr);
+    while(1)
+    {
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
+        if(client_fd < 0)
+        {
+            printf("Server: accept failed.\n");
+            exit(0);
+        }
+        inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, temp, sizeof(temp));
+        printf("Server : %s client connected.\n", temp);
+    
+        msg_size = read(client_fd, buffer, 1024);
+        write(client_fd, buffer, msg_size);
+        close(client_fd);
+        printf("Server : %s client closed.\n", temp);
+    }
+    close(server_fd);
+    return 0;
 }
