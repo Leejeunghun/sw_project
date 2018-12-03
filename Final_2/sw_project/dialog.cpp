eb7af0bec692fe448fad1bead485ad45c3ddd4b3
Leejeunghun <201401662@inu.ac.kr>
2018-12-03 08:47:47 -0800

    update betting system

--- Final_2/sw_project/dialog.cpp
+++ Final_2/sw_project/dialog.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 int betting_1=1;
 int PLAYER_COIN_1=20;
-
+int PLAYER_COIN_2=20;
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Dialog)
@@ -90,8 +90,8 @@ void Dialog::on_pushButton_Bet_clicked()
 {
     int player1=0;
     int player2=0;
-    string filePath = "oppent.txt";
-    ifstream openFile(filePath.data());
+    string filePath_1 = "oppent.txt";
+    ifstream openFile(filePath_1.data());
     if( openFile.is_open() ){
         string line;
         while(getline(openFile, line)){
@@ -102,11 +102,32 @@ void Dialog::on_pushButton_Bet_clicked()
 \
     string filePath_2 = "oppent_2.txt";
     ifstream openFile_2(filePath_2.data());
-    if( openFile.is_open() ){
+    if( openFile_2.is_open() ){    ui->lcdNumber_coin->display(PLAYER_COIN_1);
+        ui->lcdNumber_bet->display(betting_1);
         string line_2;
-        while(getline(openFile, line_2)){
+        while(getline(openFile_2, line_2)){
         player2=std::stoi( line_2 );
         }
-        openFile.close();
+        openFile_2.close();
     }
+    if(player1>player2)
+    {
+         PLAYER_COIN_1 =PLAYER_COIN_1+(betting_1*2);
+         PLAYER_COIN_2 =PLAYER_COIN_2-(betting_1*2);
+         ui->lcdNumber_coin->display(PLAYER_COIN_1);
+         ui->lcdNumber_bet->display(betting_1);
+    }
+    else if(player1<player2)
+    {
+        PLAYER_COIN_1 =PLAYER_COIN_1-(betting_1*2);
+        PLAYER_COIN_2 =PLAYER_COIN_2+(betting_1*2);
+        ui->lcdNumber_coin->display(PLAYER_COIN_1);
+        ui->lcdNumber_bet->display(betting_1);
+    }
+    else
+    {
+        ui->lcdNumber_coin->display(PLAYER_COIN_1);
+        ui->lcdNumber_bet->display(betting_1);
+    }
+
 }
