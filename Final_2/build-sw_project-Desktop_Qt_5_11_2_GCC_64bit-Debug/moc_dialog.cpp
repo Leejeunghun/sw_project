493c49ab05fb5006667068a668048cfd0ca91a4e
Juchan Kim <kjch0304@naver.com>
2018-12-08 16:10:07 +0900

    수정

---
+++ Final_2/build-sw_project-Desktop_Qt_5_11_2_GCC_64bit-Debug/moc_dialog.cpp
@@ -0,0 +1,144 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'dialog.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../sw_project/dialog.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'dialog.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Dialog_t {
+    QByteArrayData data[10];
+    char stringdata0[201];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Dialog_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Dialog_t qt_meta_stringdata_Dialog = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Dialog"
+QT_MOC_LITERAL(1, 7, 27), // "on_pushButton_Raise_clicked"
+QT_MOC_LITERAL(2, 35, 0), // ""
+QT_MOC_LITERAL(3, 36, 26), // "on_pushButton_Down_clicked"
+QT_MOC_LITERAL(4, 63, 28), // "on_pushButton_commit_clicked"
+QT_MOC_LITERAL(5, 92, 28), // "on_pushButton_Random_clicked"
+QT_MOC_LITERAL(6, 121, 25), // "on_pushButton_Bet_clicked"
+QT_MOC_LITERAL(7, 147, 26), // "on_checkBox_2_stateChanged"
+QT_MOC_LITERAL(8, 174, 4), // "arg1"
+QT_MOC_LITERAL(9, 179, 21) // "on_pushButton_clicked"
+
+    },
+    "Dialog\0on_pushButton_Raise_clicked\0\0"
+    "on_pushButton_Down_clicked\0"
+    "on_pushButton_commit_clicked\0"
+    "on_pushButton_Random_clicked\0"
+    "on_pushButton_Bet_clicked\0"
+    "on_checkBox_2_stateChanged\0arg1\0"
+    "on_pushButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Dialog[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    0,   52,    2, 0x08 /* Private */,
+       6,    0,   53,    2, 0x08 /* Private */,
+       7,    1,   54,    2, 0x08 /* Private */,
+       9,    0,   57,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void Dialog::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Dialog *_t = static_cast<Dialog *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_Raise_clicked(); break;
+        case 1: _t->on_pushButton_Down_clicked(); break;
+        case 2: _t->on_pushButton_commit_clicked(); break;
+        case 3: _t->on_pushButton_Random_clicked(); break;
+        case 4: _t->on_pushButton_Bet_clicked(); break;
+        case 5: _t->on_checkBox_2_stateChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->on_pushButton_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject Dialog::staticMetaObject = {
+    { &QDialog::staticMetaObject, qt_meta_stringdata_Dialog.data,
+      qt_meta_data_Dialog,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *Dialog::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Dialog::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Dialog.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int Dialog::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
---
+++ Final_2/build-sw_project-Desktop_Qt_5_11_2_GCC_64bit-Debug/ui_dialog.h
@@ -0,0 +1,175 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOG_H
+#define UI_DIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog
+{
+public:
+    QWidget *gridLayoutWidget;
+    QGridLayout *gridLayout;
+    QPushButton *pushButton_commit;
+    QPushButton *pushButton_Raise;
+    QPushButton *pushButton_Down;
+    QPushButton *pushButton_Random;
+    QPushButton *pushButton_Bet;
+    QPushButton *pushButton;
+    QLCDNumber *lcdNumber_bet;
+    QLCDNumber *lcdNumber_random;
+    QLCDNumber *lcdNumber_coin;
+    QLabel *label;
+    QLabel *label_2;
+    QLCDNumber *lcdNumber_gamecount;
+    QLabel *label_5;
+    QLabel *label_3;
+    QLabel *label_4;
+    QCheckBox *checkBox_2;
+    QLCDNumber *lcdNumber_player;
+
+    void setupUi(QDialog *Dialog)
+    {
+        if (Dialog->objectName().isEmpty())
+            Dialog->setObjectName(QStringLiteral("Dialog"));
+        Dialog->resize(636, 432);
+        gridLayoutWidget = new QWidget(Dialog);
+        gridLayoutWidget->setObjectName(QStringLiteral("gridLayoutWidget"));
+        gridLayoutWidget->setGeometry(QRect(0, 340, 292, 87));
+        gridLayout = new QGridLayout(gridLayoutWidget);
+        gridLayout->setSpacing(6);
+        gridLayout->setContentsMargins(11, 11, 11, 11);
+        gridLayout->setObjectName(QStringLiteral("gridLayout"));
+        gridLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton_commit = new QPushButton(gridLayoutWidget);
+        pushButton_commit->setObjectName(QStringLiteral("pushButton_commit"));
+
+        gridLayout->addWidget(pushButton_commit, 0, 2, 1, 1);
+
+        pushButton_Raise = new QPushButton(gridLayoutWidget);
+        pushButton_Raise->setObjectName(QStringLiteral("pushButton_Raise"));
+
+        gridLayout->addWidget(pushButton_Raise, 0, 0, 1, 1);
+
+        pushButton_Down = new QPushButton(gridLayoutWidget);
+        pushButton_Down->setObjectName(QStringLiteral("pushButton_Down"));
+
+        gridLayout->addWidget(pushButton_Down, 0, 1, 1, 1);
+
+        pushButton_Random = new QPushButton(gridLayoutWidget);
+        pushButton_Random->setObjectName(QStringLiteral("pushButton_Random"));
+
+        gridLayout->addWidget(pushButton_Random, 1, 0, 1, 1);
+
+        pushButton_Bet = new QPushButton(gridLayoutWidget);
+        pushButton_Bet->setObjectName(QStringLiteral("pushButton_Bet"));
+
+        gridLayout->addWidget(pushButton_Bet, 1, 1, 1, 1);
+
+        pushButton = new QPushButton(gridLayoutWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        gridLayout->addWidget(pushButton, 1, 2, 1, 1);
+
+        lcdNumber_bet = new QLCDNumber(Dialog);
+        lcdNumber_bet->setObjectName(QStringLiteral("lcdNumber_bet"));
+        lcdNumber_bet->setGeometry(QRect(20, 170, 171, 121));
+        lcdNumber_random = new QLCDNumber(Dialog);
+        lcdNumber_random->setObjectName(QStringLiteral("lcdNumber_random"));
+        lcdNumber_random->setGeometry(QRect(480, 230, 111, 91));
+        lcdNumber_random->setFrameShape(QFrame::NoFrame);
+        lcdNumber_coin = new QLCDNumber(Dialog);
+        lcdNumber_coin->setObjectName(QStringLiteral("lcdNumber_coin"));
+        lcdNumber_coin->setGeometry(QRect(500, 40, 111, 41));
+        label = new QLabel(Dialog);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(80, 120, 141, 71));
+        QFont font;
+        font.setPointSize(19);
+        label->setFont(font);
+        label_2 = new QLabel(Dialog);
+        label_2->setObjectName(QStringLiteral("label_2"));
+        label_2->setGeometry(QRect(510, 0, 131, 51));
+        QFont font1;
+        font1.setPointSize(13);
+        label_2->setFont(font1);
+        lcdNumber_gamecount = new QLCDNumber(Dialog);
+        lcdNumber_gamecount->setObjectName(QStringLiteral("lcdNumber_gamecount"));
+        lcdNumber_gamecount->setGeometry(QRect(350, 40, 101, 51));
+        label_5 = new QLabel(Dialog);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(360, 10, 67, 17));
+        label_3 = new QLabel(Dialog);
+        label_3->setObjectName(QStringLiteral("label_3"));
+        label_3->setGeometry(QRect(0, 0, 281, 101));
+        label_3->setPixmap(QPixmap(QString::fromUtf8(":/new/prefix1/title.PNG")));
+        label_4 = new QLabel(Dialog);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(480, 230, 231, 201));
+        label_4->setPixmap(QPixmap(QString::fromUtf8(":/new/prefix1/player.PNG")));
+        checkBox_2 = new QCheckBox(Dialog);
+        checkBox_2->setObjectName(QStringLiteral("checkBox_2"));
+        checkBox_2->setGeometry(QRect(210, 40, 131, 41));
+        lcdNumber_player = new QLCDNumber(Dialog);
+        lcdNumber_player->setObjectName(QStringLiteral("lcdNumber_player"));
+        lcdNumber_player->setGeometry(QRect(270, 170, 181, 111));
+        label_4->raise();
+        gridLayoutWidget->raise();
+        lcdNumber_bet->raise();
+        lcdNumber_random->raise();
+        lcdNumber_coin->raise();
+        label->raise();
+        label_2->raise();
+        lcdNumber_gamecount->raise();
+        label_5->raise();
+        label_3->raise();
+        checkBox_2->raise();
+        lcdNumber_player->raise();
+
+        retranslateUi(Dialog);
+
+        QMetaObject::connectSlotsByName(Dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog)
+    {
+        Dialog->setWindowTitle(QApplication::translate("Dialog", "Dialog", nullptr));
+        pushButton_commit->setText(QApplication::translate("Dialog", "commit", nullptr));
+        pushButton_Raise->setText(QApplication::translate("Dialog", "Raise", nullptr));
+        pushButton_Down->setText(QApplication::translate("Dialog", "Down", nullptr));
+        pushButton_Random->setText(QApplication::translate("Dialog", "Random", nullptr));
+        pushButton_Bet->setText(QApplication::translate("Dialog", "Bet", nullptr));
+        pushButton->setText(QApplication::translate("Dialog", "Oppent_betting", nullptr));
+        label->setText(QApplication::translate("Dialog", "BET coin", nullptr));
+        label_2->setText(QApplication::translate("Dialog", "Total coin", nullptr));
+        label_5->setText(QApplication::translate("Dialog", "play", nullptr));
+        label_3->setText(QString());
+        label_4->setText(QString());
+        checkBox_2->setText(QApplication::translate("Dialog", "PLayer_check", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog: public Ui_Dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOG_H
